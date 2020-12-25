eb29eb37235eb21a5a759c6aaad1bc60dc27f5c8
mert <mrtozn1923@gmail.com>
2020-12-25 17:15:27 +0300

    Lesson127 added

---
+++ 127-NPM_Basic_Usage/app.js
@@ -0,0 +1,7 @@
+//npm install lodash
+//npm uninstall lodash
+
+const _array=require('lodash/array');
+//ilk dizi de olup ikinci dizide olmayanlar
+console.log(_array.difference([1,3,4],[2,3,6]));
+
